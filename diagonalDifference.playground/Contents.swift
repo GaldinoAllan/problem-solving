7e56d34a4bfc05b4b644eba321dd81e0298ea87f
Allan <allan.g.galdino@outlook.com>
2020-07-08 09:54:34 -0300

    diagonalDifference fixed🧮

--- diagonalDifference.playground/Contents.swift
+++ diagonalDifference.playground/Contents.swift
@@ -6,14 +6,13 @@ func diagonalDifference(arr: [[Int]]) -> Int {
   var count1: Int = 0
   var count2: Int = 0
   
-  for i in 0...n {
+  for i in 0..<n {
     count1 = count1 + arr[i][i]
     count2 = count2 + arr[i][n - i - 1]
   }
   let difference = count1 - count2
   
-  return difference
+  return abs(difference)
 }
 
-// Does not run
-// Made with JavaScript
+print(diagonalDifference(arr: [[11,2,4],[4,5,6],[10,8,-12]]))
