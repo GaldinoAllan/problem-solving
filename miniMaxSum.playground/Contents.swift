5d0e954032390e54fc18d1aa3a706a9a854b0955
Allan Galdino <allan.g.galdino@outlook.com>
2020-07-08 10:48:02 -0300

    ➖MiniMaxSum➕ completed

--- miniMaxSum.playground/Contents.swift
+++ miniMaxSum.playground/Contents.swift
@@ -1,3 +1,32 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+func miniMaxSum(arr: [Int]) -> Void {
+  var max: Int = 0
+  var min: Int = 999999999999999999
+  
+  for i in arr {
+    var sum: Int = 0
+    
+    for j in arr{
+      if j == i{
+        continue
+      }
+      sum = sum + j
+    }
+    
+    if sum == 0{
+      sum = arr[0]*4
+    }
+    
+    if sum > max{
+      max = sum
+    }
+    if sum < min{
+      min = sum
+    }
+  }
+  
+  print(min, max)
+}
+
+miniMaxSum(arr: [5, 5, 5, 5, 5])
