c03d33c6d557a7c4476c27ce956c5fab09bbe5a3
Allan <allan.g.galdino@outlook.com>
2020-07-12 17:12:59 -0300

    making the core more readable

--- timeConversion.playground/Contents.swift
+++ timeConversion.playground/Contents.swift
@@ -5,11 +5,11 @@ func timeConversion(s: String) -> String {
   let time: String = s.trimmingCharacters(in: .letters)
   let splitTimeArray = time.split(separator: ":")
   var timeArray: Array<Int> = []
-  
+
   for i in splitTimeArray{
     timeArray.append(Int(i)!)
   }
-    
+
   for i in timeArray {
     if (i >= 60 || i < 0){
       return "Not a valid time"
@@ -19,32 +19,26 @@ func timeConversion(s: String) -> String {
   if (timeArray[0] > 12 || timeArray[0] < 1){
     return "Not a valid time"
   }
-  
+
   if period == "PM"{
     if timeArray[0] != 12 {
       timeArray[0] = timeArray[0] + 12
     }
-    
-    let hour = String(format:"%02d", timeArray[0])
-    let minutes = String(format:"%02d", timeArray[1])
-    let seconds = String(format:"%02d", timeArray[2])
-    
-    return "\(hour):\(minutes):\(seconds)"
-    
+
   } else if period == "AM" {
     if timeArray[0] == 12 {
       timeArray[0] = 0
     }
-    
-    let hour = String(format:"%02d", timeArray[0])
-    let minutes = String(format:"%02d", timeArray[1])
-    let seconds = String(format:"%02d", timeArray[2])
-    
-    return "\(hour):\(minutes):\(seconds)"
-    
+
   } else {
     return "Not a valid time"
   }
+
+  let hour = String(format:"%02d", timeArray[0])
+  let minutes = String(format:"%02d", timeArray[1])
+  let seconds = String(format:"%02d", timeArray[2])
+  
+  return "\(hour):\(minutes):\(seconds)"
 }
 
-print(timeConversion(s: "12:00:00AM"))
+print(timeConversion(s: "13:00:00PM"))
